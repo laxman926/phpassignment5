1a46f77bfd51fbccdbbf569bf4da8f5941cc65a6
Laxman <kumarlaxman926@gmail.com>
2020-03-29 19:14:03 -0400

    replace commnets

---
+++ part1/replacecomments.php
@@ -0,0 +1,19 @@
+<?php
+	/*
+		Plugin Name:  ReplaceString 
+		Version: 1.0
+	*/
+		function stringreplace ($str) {
+			$words = array(
+				'notgood' => '<p>Awesome article</p>', 
+				'script' => '<p>No spamming please</p>', 
+				'hi' => '<p> How are you? </p>'
+			);
+
+            $str = str_replace(array_keys($words), $words, $str); 
+            
+			return $str; // returns the value
+		}
+
+		add_filter ('pre_comment_content', 'stringreplace'); 
+?>
