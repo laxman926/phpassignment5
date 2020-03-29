0d3e59b0a15214345c2bb615e90618cbc9a94547
Laxman <kumarlaxman926@gmail.com>
2020-03-29 19:35:44 -0400

    count words

---
+++ part1/countwords.php
@@ -0,0 +1,13 @@
+<?php
+	/*
+		Plugin Name: CountWords
+		Version: 1.0
+	*/
+    // Count number of words in the content
+		function countWords ($content) {
+			return $content . " (" . str_word_count($content) . " words in the Content.)"; 
+		}
+
+		// function calls to countwords.
+		add_filter("the_content", "countWords");
+?>
